c3e6ec2a2a2c6809b6fa8e424e4864e0f51217b6
djtrin1ty <abrahamjum@gmail.com>
2017-07-26 14:46:40 -0700

    Implement basic login

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "abraham um",
+                                         email: "user@invalid",
+                                         password:              "foobar",
+                                         password_confirmation: "barbar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
+end
